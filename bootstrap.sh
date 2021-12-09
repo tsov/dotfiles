ea422ef3fd23fa60a0bc1b259916f80adec3a31f
Max Hoffmann <tsov@me.com>
2021-12-09 15:37:43 -0500

    Use agnoster for zsh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,3 +2,9 @@
 
 # Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Adds agnoster theme for Oh My Zsh
+echo "ZSH_THEME=agnoster" >> ~/.zshrc
+
+# Reloads zshrc
+source ~/.zshrc
