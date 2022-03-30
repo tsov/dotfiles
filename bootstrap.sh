9b2d390fb64832ead6d40c296f786eca44bbc6d5
Max Hoffmann <tsov@me.com>
2022-03-30 13:39:58 -0700

    Adds git-delta

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,5 +11,10 @@ source ~/.zsh/install-plugins.zsh
 # Custom bin folder
 ln -sfn ~/dotfiles/bin ~/.bin
 
+# Git Delta
+curl -k -L -s --compressed https://github.com/dandavison/delta/releases/download/0.12.1/git-delta_0.12.1_$(dpkg --print-architecture).deb > ~/git-delta.deb
+sudo dpkg -i ~/git-delta.deb
+ln -sfn ~/dotfiles/git/config ~/.gitconfig
+
 # Reloads zshrc
 source ~/.zshrc
